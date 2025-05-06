d4a8c49d300aabd6f37ef1cf199c2c7fccd36188
fvilla12 <148190016+fvilla12@users.noreply.github.com>
2025-05-06 16:54:38 +0200

    Update fix_colors.py

--- fix_colors.py
+++ fix_colors.py
@@ -1,17 +1,1 @@
-from app import app, db
-from models import ImagenesScala
-import ast
-import json
 
-with app.app_context():
-    registros = ImagenesScala.query.all()
-    for r in registros:
-        if isinstance(r.colors, str):
-            try:
-                # Intenta convertir el string a dict
-                dict_color = ast.literal_eval(r.colors)
-                r.colors = dict_color
-                print(f"Corrigiendo registro {r.id}")
-            except Exception as e:
-                print(f"Error en registro {r.id}: {e}")
-    db.session.commit()
